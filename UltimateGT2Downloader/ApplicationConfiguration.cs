30e6cb0e4d821f4e84b8c0c5efcec43eede4e3f9
hadi12d <hadiherehesh@gmail.com>
2024-04-15 12:39:55 -0700

    commit UGT2D V2

---
+++ UltimateGT2Downloader/ApplicationConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.CompilerServices;
+using System.Windows.Forms;
+
+namespace Ultimate_GT2_Downloader
+{
+    [CompilerGenerated]
+    internal static class ApplicationConfiguration
+    {
+        public static void Initialize()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(defaultValue: false);
+        }
+    }
+}
